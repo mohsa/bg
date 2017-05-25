128665f9262e2ed4d5eb6b8f0c0ad3ff06ee1222
Mohammad Saifullah <saif.phy@gmail.com>
2017-05-25 11:57:40 +0200

    Update run_in_docker.sh

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,8 +1,8 @@
 nvidia-docker run --rm \
 -e DISPLAY=$DISPLAY \
 -ti \
--v /home/ubuntu/disk:/home/ubuntu \
+-v /home/ubuntu/disk1:/home/ubuntu \
 --net=host \
 -w /home/ubuntu/background_display \
-dl:1.0 \
+dl:1.1 \
 python3 -m homography.deep_homo_tf
