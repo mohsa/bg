879174a49f3535efbe991bd2e42f1210ce124a01
Mohammad Saifullah <saif.phy@gmail.com>
2017-05-25 11:56:55 +0200

    Update download_images.sh

--- Data/Images/download_images.sh
+++ Data/Images/download_images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aws s3 cp s3-eu-west-1.amazonaws.com/signality.football.background.play1.360p/frames_360p.zip .
-unzip -o -j -qq frames_360.zip
-rm frames_360.zip
+wget https://s3-eu-west-1.amazonaws.com/signality.football.background.play1.360p/frames_360p.zip
+unzip -o -qq frames_360p.zip
+rm frames_360p.zip
 
